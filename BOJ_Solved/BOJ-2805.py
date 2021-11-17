cefe0c9eeceb230f1fe670896cda3561462461ab
CodingLeeSeungHoon <free_minkya@naver.com>
2021-11-18 01:24:11 +0900

    [BOJ-2805] 나무 자르기

---
+++ BOJ_Solved/BOJ-2805.py
@@ -0,0 +1,22 @@
+"""
+백준 2805번 : 나무 자르기
+이분 탐색으로 해결하기~!
+"""
+from sys import stdin
+
+n, m = map(int, stdin.readline().split())
+
+trees = list(map(int, stdin.readline( ).split( )))
+start = 0
+end = max(trees)
+
+while start <= end:
+    mid = (start + end) // 2
+    cnt = sum(t - mid if t > mid else 0 for t in trees)
+
+    if cnt >= m:
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(end)
