7ec5373b8d70c5b15406baf14e177cbde2ff753d
CodingLeeSeungHoon <free_minkya@naver.com>
2021-11-19 03:32:06 +0900

    [GraphTheory] bfs_basic

---
+++ Graph Theory/bfs_basic.py
@@ -0,0 +1,13 @@
+# BFS 너비 우선 탐색 Algorithm의 기본형
+from collections import deque
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
