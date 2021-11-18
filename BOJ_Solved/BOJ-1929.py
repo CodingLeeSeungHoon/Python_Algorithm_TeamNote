9945c3feeb8d175f27e13fc07d6acca318115ac7
CodingLeeSeungHoon <free_minkya@naver.com>
2021-11-19 03:14:47 +0900

    [BOJ-1929] 소수 구하기

---
+++ BOJ_Solved/BOJ-1929.py
@@ -0,0 +1,23 @@
+"""
+백준 1929번 : 소수 구하기
+"""
+
+a, b = map(int, input( ).split( ))
+
+def is_prime(a, b):
+    prime = [True] * b
+    prime[1] = False
+
+    m = int(b ** 0.5)
+    for i in range(2, m + 1):
+        if prime[i]:
+            for j in range(i + i, b, i):
+                prime[j] = False
+
+    return [i for i in range(a, b) if prime[i]]
+
+
+prime = is_prime(a, b + 1)
+
+for p in prime:
+    print(p)
