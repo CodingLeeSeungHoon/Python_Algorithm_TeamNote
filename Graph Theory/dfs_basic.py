5330e7f0b91a15755a4420bc294ac895568d1506
CodingLeeSeungHoon <free_minkya@naver.com>
2021-11-19 03:31:33 +0900

    [GraphTheory] dfs_basic

---
+++ Graph Theory/dfs_basic.py
@@ -0,0 +1,7 @@
+# DFS 깊이 우선 탐색 Algorithm의 기본형
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
