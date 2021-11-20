6a878eb3cdc4674480e4499b89dcd748d3c2f0d0
CodingLeeSeungHoon <free_minkya@naver.com>
2021-11-20 21:37:26 +0900

    [GraphTheory] Binary Search Tree Traversal

---
+++ Graph Theory/BST_Traversal.py
@@ -0,0 +1,36 @@
+n = int(input( ))
+tree = {}
+
+def preorder(node):
+    if node == '.':
+        return
+    print(node, end='')
+    preorder(tree[node][0])
+    preorder(tree[node][1])
+
+
+def inorder(node):
+    if node == '.':
+        return
+    inorder(tree[node][0])
+    print(node, end='')
+    inorder(tree[node][1])
+
+
+def pastorder(node):
+    if node == '.':
+        return
+    pastorder(tree[node][0])
+    pastorder(tree[node][1])
+    print(node, end='')
+
+
+for _ in range(n):
+    node, left, right = input( ).split( )
+    tree[node] = (left, right)
+
+preorder('A')
+print( )
+inorder('A')
+print( )
+pastorder('A')
