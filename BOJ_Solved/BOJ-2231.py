17dc273994ef64e4343c61fc7fa8537116c6e279
CodingLeeSeungHoon <free_minkya@naver.com>
2021-11-20 22:02:12 +0900

    [BOJ-2231] 분해합

---
+++ BOJ_Solved/BOJ-2231.py
@@ -0,0 +1,28 @@
+"""
+백준 2231번 : 분해합
+"""
+natural_number = int(input( ))
+result = 0
+
+
+def is_generator_number(str_num, natural_number):
+    length = len(str_num)
+    temp = 0
+    for index in range(length):
+        temp += int(str_num[index])
+
+    temp += int(str_num)
+
+    if temp == natural_number:
+        return True
+    else:
+        return False
+
+
+for loops in range(1, natural_number + 1):
+    if is_generator_number(str(loops), natural_number):
+        result = loops
+        break
+
+print(result)
+​
