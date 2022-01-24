bd2770bc8303aacab62d58ced6cb01cc31f44f1c
CodingLeeSeungHoon <free_minkya@naver.com>
2022-01-24 20:34:53 +0900

    [BOJ-11444] 피보나치 수 6

---
+++ BOJ_Solved/BOJ-11444.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+"""
+백준 11444번 : 피보나치 수 6
+"""
+
+import sys
+sys.setrecursionlimit(100000)
+input = sys.stdin.readline
+fibonacci = {0: 0, 1: 1, 2: 1}
+
+
+def getFibonacci(n):
+    if not fibonacci.get(n):
+        if n % 2 == 1:
+            fibonacci[n] = (getFibonacci((n+1)//2)**2 +
+                            getFibonacci((n+1)//2-1)**2) % 1000000007
+        else:
+            fibonacci[n] = (getFibonacci(n+1) - getFibonacci(n-1)) % 1000000007
+
+    return fibonacci[n]
+
+
+N = int(input())
+print(getFibonacci(N))
