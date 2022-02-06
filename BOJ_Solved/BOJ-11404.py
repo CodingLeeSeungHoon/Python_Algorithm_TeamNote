c791f427ab5003326d2bfd5802746318e20e1989
CodingLeeSeungHoon <free_minkya@naver.com>
2022-02-07 00:03:50 +0900

    [BOJ-11404] 플로이드

---
+++ BOJ_Solved/BOJ-11404.py
@@ -0,0 +1,30 @@
+"""
+백준 11404번 : 플로이드
+- 플로이드-워셜 알고리즘
+"""
+import sys
+input = sys.stdin.readline
+n = int(input())
+m = int(input())
+bus_cost = [[1000001 for _ in range(n+1)] for _ in range(n+1)]
+
+for _ in range(m):
+    start, end, cost = map(int, input().split())
+    bus_cost[start][end] = min(cost, bus_cost[start][end])
+
+# 플로이드 워셜 알고리즘
+for k in range(1, n+1):  # 경로 for문이 가장 상위 단계여야 누락되지 않는다
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            if i == j:  # 자기 자신으로 오는 경우는 없다고 했음
+                bus_cost[i][j] = 0
+            else:  # 경로 거치는 것 / 직접 가는 것 / 이전 경로들
+                bus_cost[i][j] = min(bus_cost[i][j], bus_cost[i][k] + bus_cost[k][j])
+
+for row in bus_cost[1:]:
+    for col in row[1:]:
+        if col == 1000001:
+            print(0, end=" ")
+        else:
+            print(col, end=" ")
+    print()
