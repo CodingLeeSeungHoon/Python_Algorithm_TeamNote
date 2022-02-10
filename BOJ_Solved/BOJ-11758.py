fb0960356e4da7276268ba2e547d121395844d55
CodingLeeSeungHoon <free_minkya@naver.com>
2022-02-10 23:14:53 +0900

    [BOJ-11758] CCW

---
+++ BOJ_Solved/BOJ-11758.py
@@ -0,0 +1,22 @@
+"""
+백준 11758번 : CCW
+"""
+
+p1 = list(map(int, input().split()))
+p2 = list(map(int, input().split()))
+p3 = list(map(int, input().split()))
+
+def ccw(p1, p2, p3):
+    x1, y1 = p1
+    x2, y2 = p2
+    x3, y3 = p3
+
+    return (x1 * y2) + (x2 * y3) + (x3 * y1) - (y1 * x2) - (y2 * x3) - (y3 * x1)
+
+result = ccw(p1, p2, p3)
+if result > 0:
+    print(1)
+elif result < 0:
+    print(-1)
+else:
+    print(0)
