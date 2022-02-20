37c1b0d8f86353f35e9c1e9674b79ebe04b1a8f7
CodingLeeSeungHoon <free_minkya@naver.com>
2022-02-20 21:00:58 +0900

    [BOJ-1300] K번째 수

---
+++ BOJ_Solved/BOJ-1300.py
@@ -0,0 +1,23 @@
+"""
+백준 1300번 : K번째 수
+"""
+
+n = int(input())
+k = int(input())
+
+def binary_search(target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+
+        cnt = 0
+        for i in range(1, n+1):
+            cnt += min(mid//i, n)
+
+        if cnt >= target:
+            end = mid-1
+        else:
+            start = mid+1
+    return start
+
+
+print(binary_search(k, 1, n*n))
