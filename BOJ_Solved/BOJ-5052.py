a34219f73c229e23aec915fca777007f49cb23de
CodingLeeSeungHoon <free_minkya@naver.com>
2022-02-21 21:31:02 +0900

    [BOJ-5052] 전화번호 목록

---
+++ BOJ_Solved/BOJ-5052.py
@@ -0,0 +1,21 @@
+"""
+백준 5052번 : 전화번호 목록
+"""
+
+t = int(input())
+
+def check():
+    for i in range(len(a)-1):
+        if a[i] == a[i+1][0:len(a[i])]:
+            print('NO')
+            return
+    print('YES')
+
+
+for _ in range(t):
+    n = int(input())
+    a = []
+    for i in range(n):
+        a.append(input().strip())
+    a.sort()
+    check()
