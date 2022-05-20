467333bfdfc32423ac83871044b56e55caedb022
CodingLeeSeungHoon <free_minkya@naver.com>
2022-05-20 19:39:46 +0900

    [BOJ-8718] Balwanek

---
+++ BOJ_Solved/BOJ-8718.py
@@ -0,0 +1,12 @@
+"""
+백준 8718번 : Balwanek
+"""
+x, k = map(int, input().split())
+if k*7 <= x:
+    print(k*7000)
+elif k*3.5 <= x:
+    print(k*3500)
+elif k*1.75 <= x:
+    print(k*1750)
+else:
+    print(0)
